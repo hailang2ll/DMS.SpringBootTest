8bce02c3ad3765c0b7fdd262cf958e514f84717c
hailang2ll <79522860@qq.com>
2022-03-03 19:49:50 +0800

    init

---
+++ dms-admin/src/main/java/com/dms/admin/DMSApplication.java
@@ -0,0 +1,12 @@
+package com.dms.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DMSApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DMSApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  系统启动成功   ლ(´ڡ`ლ)ﾞ  \n");
+    }
+}
---
+++ dms-admin/src/main/java/com/dms/admin/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.dms.admin.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/getInfo")
+    public String getInfo()
+    {
+        return "成功";
+    }
+}
